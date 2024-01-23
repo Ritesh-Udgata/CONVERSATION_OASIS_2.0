f7a8906acd9e3a28e612c17710761ae8babf673e
Ritesh Udgata <86166283+Ritesh-Udgata@users.noreply.github.com>
2024-01-24 04:00:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
+const express = require('express');
+const app = express();
+
+const http = require('http').Server(app);
+
+const userRoute = require('./routes/userRoute');
+
+const User = require('./models/userModel');
+
+const Chat = require('./models/chatModel');
+
+app.use('/', userRoute);
+app.use(express.static('public'));
+
+const io = require('socket.io')(http);
+var usp = io.of('/user-namespace');
+
+usp.on('connection', async function(socket){
+    console.log('User connected');
+
+    console.log(socket.handshake.auth.token);
+
+    var userId = socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate({_id: userId}, { $set: { is_online: '1'}});
+
+    //user broadcast online status
+    socket.broadcast.emit('getOnlineUser', {user_id: userId});
+
+     
+
+
+    
+
+
+    socket.on('disconnect', async function(){
+        console.log('user Disconnected');
+
+        var userId = socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate({_id: userId}, { $set: { is_online: '0'}});
+
+    //user broadcast offline status
+    socket.broadcast.emit('getOfflineUser', {user_id: userId});
+
+        
+
+    });
+
+    socket.on('newChat', function(data){
+        socket.broadcast.emit('loadNewChat', data);
+    });
+
+    socket.on('existsChat', async function(data){
+        var chats = await Chat.find({
+            $or:[
+                {
+                    sender_id:data.sender_id, receiver_id:data.receiver_id
+                },
+                {
+                    sender_id:data.receiver_id, receiver_id:data.sender_id
+                },
+            ]
+        });
+
+        socket.emit('loadChats', {chats: chats});
+    
+
+});
+
+
+
+
+
+http.listen(3000, function(){
+
+    console.log('Server is running');
+
+});
+
